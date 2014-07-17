1b1d6b4425f101609cb3e62b0352f418976426fe
Martin N <mr.avgp@googlemail.com>
2014-07-17 18:26:06 +0000

    Adding the layers to the editor

--- js/editor.js
+++ js/editor.js
@@ -8,7 +8,7 @@ var editor   = document.getElementById("editor"),
     iso      = new Isomer(editor),
     canvas   = new Canvas(editor);
 
-var model = [];
+var model = [], currentLayer = 0;
 
 function initModel(rows, cols, levels) {
     for(var lvl=0; lvl < levels; lvl++) {
@@ -28,7 +28,7 @@ function initModel(rows, cols, levels) {
 function drawGrid(tilesPerRow, tilesPerCol) {
     for(var y=0; y < tilesPerRow; y++) {
         for(var x=0; x < tilesPerCol; x++) {
-            var point = new Point (x, y, 0);
+            var point = new Point (x, y, currentLayer);
             iso.add(Shape.Prism(point, 1, 1, 0.1), new Color(200, 200, 200, 0.5));
         }
     }
@@ -44,8 +44,8 @@ function setupControl(tilesPerRow, tilesPerCol) {
                 controlTile.className = "controlTile";
                 controlTile.addEventListener("click", function() {
                     console.log("Tile " + tx + ", " + ty);
-                    model[0][tx][ty] = !model[0][tx][ty];
-                    if(model[0][tx][ty]) {
+                    model[currentLayer][tx][ty] = !model[currentLayer][tx][ty];
+                    if(model[currentLayer][tx][ty]) {
                         this.classList.add("set"); 
                     } else {
                         this.classList.remove("set");
@@ -60,19 +60,38 @@ function setupControl(tilesPerRow, tilesPerCol) {
 
 function render() {
     canvas.clear();
-    drawGrid(10, 10);
-    for(var y=9;y >= 0; y--) {
-        for(var x=9; x >= 0; x--) {
-            if(model[0][x][9-y]) {
-                var point = new Point(x, y, 0);
-                iso.add(Shape.Prism(point, 1, 1, 1), new Color(200, 200, 200));
+    iso.add(Shape.Prism(Point.ORIGIN, 10, 10, 10), new Color(200, 200, 200, 0.1))
+    for(var z=0; z < 9; z++) {
+        if(z == currentLayer) {
+            drawGrid(10, 10);
+        }
+
+        for(var y=9;y >= 0; y--) {
+            for(var x=9; x >= 0; x--) {
+                if(model[z][x][9-y]) {
+                    var point = new Point(x, y, z);
+                    var color = (z == currentLayer ? new Color(200, 200, 200) : new Color(200, 200, 200, 0.5))
+                    iso.add(Shape.Prism(point, 1, 1, 1), new Color(200, 200, 200));
+                }
             }
         }
     }
-    
+
     requestAnimationFrame(render);
 }
 
 setupControl(10, 10);
 initModel(10,10,10);
 render();
+
+document.getElementById("layerUp").addEventListener("click", function() {
+    if(currentLayer < 9) {
+        currentLayer++;
+    }
+});
+
+document.getElementById("layerDown").addEventListener("click", function() {
+    if(currentLayer > 0) {
+        currentLayer--;
+    }
+});
