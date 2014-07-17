902451a404a3f172833cb0ad2c68a99454473962
Martin N <mr.avgp@googlemail.com>
2014-07-17 06:21:53 +0000

    Removed useless renderer abstraction, fixed perspective issues

--- js/editor.js
+++ js/editor.js
@@ -6,19 +6,35 @@ var Path   = Isomer.Path,
 
 var editor   = document.getElementById("editor"),
     iso      = new Isomer(editor),
-    renderer = { isomer: iso, path: Path, color: Color, shape: Shape, canvas: Canvas, point: Point };
     canvas   = new Canvas(editor);
 
-function drawGrid(render, tilesPerRow, tilesPerCol) {
+var model = [];
+
+function initModel(rows, cols, levels) {
+    for(var lvl=0; lvl < levels; lvl++) {
+        var level = [];
+        for(var r=0; r < rows; r++) {
+            var row = [];
+            for(var c=0; c < cols; c++) {
+                row.push(false);
+            }
+            level.push(row);
+        }
+        model.push(level);
+    }
+    return model;
+}
+
+function drawGrid(tilesPerRow, tilesPerCol) {
     for(var y=0; y < tilesPerRow; y++) {
         for(var x=0; x < tilesPerCol; x++) {
-            var point = new render.point (x, y, 0);
-            render.isomer.add(render.shape.Prism(point, 1, 1, 0.1), new render.color(200, 200, 200, 0.5));
+            var point = new Point (x, y, 0);
+            iso.add(Shape.Prism(point, 1, 1, 0.1), new Color(200, 200, 200, 0.5));
         }
     }
 }
 
-function setupControl(render, tilesPerRow, tilesPerCol) {
+function setupControl(tilesPerRow, tilesPerCol) {
     var controlContainer = document.getElementById("control");
     for(var y=0; y < tilesPerRow; y++) {
         var rowControl = document.createElement("div");
@@ -26,11 +42,15 @@ function setupControl(render, tilesPerRow, tilesPerCol) {
             (function(tx, ty, row) {
                 var controlTile = document.createElement("div");
                 controlTile.className = "controlTile";
-                controlTile.onclick = function() {
+                controlTile.addEventListener("click", function() {
                     console.log("Tile " + tx + ", " + ty);
-                    var point = new render.point(tx, ty, 0);
-                    render.isomer.add(render.shape.Prism(point, 1, 1, 1), new render.color(200, 200, 200));
-                }
+                    model[0][tx][ty] = !model[0][tx][ty];
+                    if(model[0][tx][ty]) {
+                        this.classList.add("set"); 
+                    } else {
+                        this.classList.remove("set");
+                    }
+                }, false);
                 row.appendChild(controlTile);
             })(x, y, rowControl);
         }
@@ -38,5 +58,21 @@ function setupControl(render, tilesPerRow, tilesPerCol) {
     }
 }
 
-drawGrid(renderer, 10, 10);
-setupControl(renderer, 10, 10);
+function render() {
+    canvas.clear();
+    drawGrid(10, 10);
+    for(var y=9;y >= 0; y--) {
+        for(var x=9; x >= 0; x--) {
+            if(model[0][x][9-y]) {
+                var point = new Point(x, y, 0);
+                iso.add(Shape.Prism(point, 1, 1, 1), new Color(200, 200, 200));
+            }
+        }
+    }
+    
+    requestAnimationFrame(render);
+}
+
+setupControl(10, 10);
+initModel(10,10,10);
+render();
